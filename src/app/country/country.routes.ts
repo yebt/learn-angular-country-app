2c0a278da4ef39b8b32050fc11bfc363581fd2b4
Yahir <yahir.eduardo.bravo@gmail.com>
2026-01-18 22:34:43 -0500

    feat(s9-c6): layouts

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
 
 const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent, // deafult vuiew no lazy
+      },
+
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ],
   },
 ];
 
---
+++ src/app/country/layouts/country-layout/country-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [RouterOutlet],
+  templateUrl: './country-layout.component.html',
+})
+export class CountryLayoutComponent {
+
+}
